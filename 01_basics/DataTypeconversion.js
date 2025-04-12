dcc9e86bd3045f561ff66193ed1c99b0bdeece64
Ashish Kumar Singh <aks06012004@gmail.com>
2025-04-12 03:30:57 +0000

    Conersion of Data types

---
+++ 01_basics/DataTypeconversion.js
@@ -0,0 +1,26 @@
+// let number="5"+2;
+// console.log(number);
+// let score="Ashish888";
+// console.log(typeof score)
+// console.log(score);
+// let s=true;
+// console.log(typeof (s));
+// let t="6"-4;
+// console.log(typeof t);
+// console.log(t);
+// let score2=null;
+// console.log(typeof score2);
+// let he=true;
+// console.log(he);
+// console.log(typeof he);
+// let a = Number("123");           // 123
+//   let b = Number("abc");           // NaN
+//   let c = parseInt("123.45");      // 123
+//   let d = parseFloat("123.45");
+//   console.log(b)
+//   console.log(c)
+//   console.log(typeof b);
+let somenumber=17;
+let stringnumber=String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber)
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+let name="Ashish Singh"
+let age=25;
+let isstudent=true;
+let city;
+let salary=null;
+console.log(name);
+console.log(age);
+console.log(typeof age);//it define the type of number

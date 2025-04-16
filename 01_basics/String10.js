f50ec4835cd6b9f2a7b90fc7d2c367608d0034d8
Ashish Kumar Singh <aks06012004@gmail.com>
2025-04-16 05:10:47 +0000

    String

---
+++ 01_basics/String10.js
@@ -0,0 +1,10 @@
+const name="Ashish Singh"
+const age=23;
+const usn=17;
+//console.log(`Hello my name is${name} age is${age} and my usn is${usn}`);//This in modern way to add two string
+// console.log(name.charAt(7));
+// console.log(name.indexOf('i'));
+// console.log(name.substring(0,3));
+const newstring="    Ashish           ";
+console.log(newstring);
+console.log(newstring.trim());

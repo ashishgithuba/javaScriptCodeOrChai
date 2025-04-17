6a169c2acc2b4b86076e955698a4a2f6a57191e5
Ashish Kumar Singh <aks06012004@gmail.com>
2025-04-17 18:07:26 +0000

    Date Time

---
+++ 01_basics/DateandTime.js
@@ -0,0 +1,10 @@
+let myDate=new Date("2025-04-16");
+//console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+ //console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+console.log(myDate.getDate());
+console.log(myDate.getHours());
+console.log(myDate.getMonth()+1);
---
+++ 01_basics/NumberandMaths12.js
@@ -0,0 +1,3 @@
+//let a=86.9;
+console.log(Math.round(86.9));
+console.log(Math.random());

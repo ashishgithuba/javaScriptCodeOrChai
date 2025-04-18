685615f3e395cb1b32fe5785c8516ca540c650f7
Ashish Kumar Singh <aks06012004@gmail.com>
2025-04-18 06:31:29 +0000

    Basics of Arrays

---
+++ 02_basics/Arrays.js
@@ -0,0 +1,11 @@
+const myArray=[3,8,9,10,23]
+//console.log(myArray[4]);
+const myHero=['Ashish','Raj','ravi','Hrathik','Ajya']
+//console.log(myHero[4]);
+//myArray.push(6)//vale add in arrays
+//myArray.push(12)
+//myArray.pop()//Remove the last element
+myArray.unshift(99)//Add krta ahi
+myArray.shift()//Remove karta hai
+console.log(myArray);
+console.log(myArray.includes(9));

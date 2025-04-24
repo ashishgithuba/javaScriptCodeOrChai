3d025468e41324f499eb17b6506cfa4803bbfa8c
Ashish Kumar Singh <aks06012004@gmail.com>
2025-04-24 13:00:42 +0000

    object

---
+++ 03_object/basicobject.js
@@ -0,0 +1,12 @@
+const person={
+name:"Ashish Kumar",
+age:24,
+isstudent:true,
+email:"aks"
+
+}
+console.log(person.age);
+console.log(person["email"]);
+// person.email="ashish";
+// Object.freeze(person);
+// console.log(person["email"]);

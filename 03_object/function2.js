13e88a800d01488470d6d9faa4af906fbd5728f3
Ashish Kumar Singh <aks06012004@gmail.com>
2025-04-29 05:01:35 +0000

    Function conditions

---
+++ 03_object/function2.js
@@ -0,0 +1,10 @@
+function userlogingmessage(Username)
+{
+    if(Username==undefined)
+    {
+        console.log("Please ernter the User name");
+    }
+    return
+
+}
+console.log(userlogingmessage());

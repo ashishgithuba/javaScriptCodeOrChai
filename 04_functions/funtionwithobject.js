1b714847ba68fb56b673d13b1de5dc25dd3c9a11
Ashish Kumar Singh <aks06012004@gmail.com>
2025-05-01 03:31:07 +0000

    function with object

---
+++ 04_functions/funtionwithobject.js
@@ -0,0 +1,11 @@
+function loginuserMessage(username="Ashish")
+{
+    if(!username)
+    {
+        console.log("Please Enter the User name")
+    return
+}
+return '${Ashish} just logged in'
+
+}
+console.log(loginuserMessage()); 

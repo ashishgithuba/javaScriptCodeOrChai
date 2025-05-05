19f98790eeefa6f9e922d2bc941e1ce5e74f1db4
Ashish Kumar Singh <aks06012004@gmail.com>
2025-05-05 06:46:39 +0000

    loops

---
+++ ControlFlow/Loops/Forloops.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++) {
+    const element=i;
+    if(element==7)
+    {
+        console.log("7 is the best number");
+    }
+    console.log(element);
+  }
+  
---
+++ ControlFlow/switch.js
@@ -0,0 +1,11 @@
+let day = "Monday";
+switch (day) {
+  case "Monday":
+    console.log("Start of the week");
+    break;
+  case "Friday":
+    console.log("Weekend is coming");
+    break;
+  default:
+    console.log("Just another day");
+}

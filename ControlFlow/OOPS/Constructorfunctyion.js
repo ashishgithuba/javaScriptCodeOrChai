930cfa9b375b69b44570c8366fc419742702441e
Ashish Kumar Singh <aks06012004@gmail.com>
2025-05-21 09:21:02 +0530

    Constructorfunction.js

--- ControlFlow/OOPS/Constructorfunctyion.js
+++ ControlFlow/OOPS/Constructorfunctyion.js
@@ -3,7 +3,8 @@ function user(username,loginCOunt,isLoggedInn)
     this.username=username;
     this.loginCOunt=loginCOunt;
     this.isLoggedInn=isLoggedInn;
-    return this
+   // return this
+    //if i am removing this keyword it is not effect it will print  
 
 }
 const userOne=new user("Ashish kumar singh",12,true)
